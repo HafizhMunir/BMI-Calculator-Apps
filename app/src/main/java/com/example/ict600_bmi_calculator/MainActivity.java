47e11b5a4e512648ca362e4dd76dd88bfee2cb60
HafizhMunir <muhammadhafizhmohdmunir@gmail.com>
2021-07-11 15:10:08 +0800

    BMI Calculator Apps

---
+++ app/src/main/java/com/example/ict600_bmi_calculator/MainActivity.java
@@ -0,0 +1,149 @@
+package com.example.ict600_bmi_calculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.ToggleButton;
+
+import static com.example.ict600_bmi_calculator.BMICal.BMI_CATEGORY_HEALTHY;
+import static com.example.ict600_bmi_calculator.BMICal.BMI_CATEGORY_OBESE;
+import static com.example.ict600_bmi_calculator.BMICal.BMI_CATEGORY_OBESE1;
+import static com.example.ict600_bmi_calculator.BMICal.BMI_CATEGORY_OBESE2;
+import static com.example.ict600_bmi_calculator.BMICal.BMI_CATEGORY_OVERWEIGHT;
+import static com.example.ict600_bmi_calculator.BMICal.BMI_CATEGORY_UNDERWEIGHT;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText weightKgEditText, heightCmEditText;
+    private EditText weightLbsEditText, heightFtEditText, heightInEditText;
+    private ToggleButton toggleUnitsButton;
+    private CardView bmiResultCardView;
+    private TextView bmiTextView, categoryTextView;
+    private Button button;
+    private Button calculateButton;
+    private boolean inMetricUnits;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button = (Button) findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openAboutUs();
+            }
+        });
+
+        weightKgEditText = findViewById(R.id.activity_main_weightkgs);
+        heightCmEditText = findViewById(R.id.activity_main_heightcm);
+
+        weightLbsEditText = findViewById(R.id.activity_main_weightlbs);
+        heightFtEditText = findViewById(R.id.activity_main_heightfeet);
+        heightInEditText = findViewById(R.id.activity_main_heightinches);
+
+        calculateButton = findViewById(R.id.activity_main_calculate);
+        toggleUnitsButton = findViewById(R.id.activity_main_toggleunits);
+
+        bmiTextView = findViewById(R.id.activity_main_bmi);
+        categoryTextView = findViewById(R.id.activity_main_category);
+        bmiResultCardView = findViewById(R.id.activity_main_resultcard);
+
+        inMetricUnits = true;
+        updateInputsVisibility();
+        bmiResultCardView.setVisibility(View.GONE);
+
+        calculateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (inMetricUnits) {
+                    if (weightKgEditText.length() == 0 || heightCmEditText.length() == 0) {
+                        Toast.makeText(MainActivity.this, "Please Enter Weight and Height to Calculate BMI", Toast.LENGTH_SHORT).show();
+                    } else {
+                        double heightInCms = Double.parseDouble(heightCmEditText.getText().toString());
+                        double weightInKgs = Double.parseDouble(weightKgEditText.getText().toString());
+                        double bmi = BMICal.getInstance().calculateBMIMetric(heightInCms, weightInKgs);
+                        displayBMI(bmi);
+                    }
+                } else {
+                    if (weightLbsEditText.length() == 0 || heightFtEditText.length() == 0 || heightInEditText.length() == 0) {
+                        Toast.makeText(MainActivity.this, "Please Enter Weight and Height to Calculate BMI", Toast.LENGTH_SHORT).show();
+                    } else {
+                        double heightFeet = Double.parseDouble(heightFtEditText.getText().toString());
+                        double heightInches = Double.parseDouble(heightInEditText.getText().toString());
+                        double weightLbs = Double.parseDouble(weightLbsEditText.getText().toString());
+                        double bmi = BMICal.getInstance().calculateBMIImperial(heightFeet, heightInches, weightLbs);
+                        displayBMI(bmi);
+                    }
+                }
+            }
+        });
+
+        toggleUnitsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                inMetricUnits = !inMetricUnits;
+                updateInputsVisibility();
+            }
+        });
+    }
+
+    private void updateInputsVisibility() {
+        if (inMetricUnits) {
+            heightCmEditText.setVisibility(View.VISIBLE);
+            weightKgEditText.setVisibility(View.VISIBLE);
+            heightFtEditText.setVisibility(View.GONE);
+            heightInEditText.setVisibility(View.GONE);
+            weightLbsEditText.setVisibility(View.GONE);
+        } else {
+            heightCmEditText.setVisibility(View.GONE);
+            weightKgEditText.setVisibility(View.GONE);
+            heightFtEditText.setVisibility(View.VISIBLE);
+            heightInEditText.setVisibility(View.VISIBLE);
+            weightLbsEditText.setVisibility(View.VISIBLE);
+        }
+    }
+
+    private void displayBMI(double bmi) {
+        bmiResultCardView.setVisibility(View.VISIBLE);
+
+        bmiTextView.setText(String.format("%.1f", bmi));
+
+        String bmiCategory = BMICal.getInstance().classifyBMI(bmi);
+        categoryTextView.setText(bmiCategory);
+
+        switch (bmiCategory) {
+            case BMI_CATEGORY_UNDERWEIGHT:
+                bmiResultCardView.setCardBackgroundColor(Color.YELLOW);
+                break;
+            case BMI_CATEGORY_HEALTHY:
+                bmiResultCardView.setCardBackgroundColor(Color.GREEN);
+                break;
+            case BMI_CATEGORY_OVERWEIGHT:
+                bmiResultCardView.setCardBackgroundColor(Color.YELLOW);
+                break;
+            case BMI_CATEGORY_OBESE:
+                bmiResultCardView.setCardBackgroundColor(Color.RED);
+                break;
+            case BMI_CATEGORY_OBESE1:
+                bmiResultCardView.setCardBackgroundColor(Color.RED);
+                break;
+            case BMI_CATEGORY_OBESE2:
+                bmiResultCardView.setCardBackgroundColor(Color.RED);
+                break;
+        }
+    }
+
+    public void openAboutUs() {
+        Intent intent = new Intent(this, AboutUs.class);
+        startActivity(intent);
+    }
+}
